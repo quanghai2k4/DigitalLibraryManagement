e8a927761f4743d5e200ed4a4a41afa29e82df4d
Dylan <quanghai2k4.git@gmail.com>
2025-03-12 13:31:21 +0700

    add .gitignore

--- obj/Debug/net9.0-windows/DigitalLibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/DigitalLibraryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalLibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3239ffcb56804d9121d103f6e5f4fa521b6afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalLibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalLibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
